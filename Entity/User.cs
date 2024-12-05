9c695351cacc9b4a73a8a1e19bf45aa14e5112b7
Sena Durgut <91386955+senadurgutt@users.noreply.github.com>
2024-12-05 15:26:50 +0300

    entityler olusturuldu

---
+++ Entity/User.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1.Entity
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string? UserName { get; set; }
+        public List<Post> Posts { get; set; } = new List<Post>();
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+
+    }
+}
