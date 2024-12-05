83533eca18ab5958f21fe2e90811ec60bdbfe028
Sena Durgut <91386955+senadurgutt@users.noreply.github.com>
2024-12-05 17:45:11 +0300

    db bağlantısı sağlandı

--- Program.cs
+++ Program.cs
@@ -1,9 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data.Concrete.EfCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+builder.Services.AddDbContext<BlogContext>(options =>
+{
+    var config = builder.Configuration;
+    var connectionString = config.GetConnectionString("database");
+    options.UseSqlServer(connectionString);
+});
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
